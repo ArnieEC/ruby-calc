dbb480b2bf124c05e9735f9185334cfa6a74f823
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2018-12-18 23:04:26 +0000

    added advanced calculator functions (root and square)

--- calc.rb
+++ calc.rb
@@ -59,6 +59,16 @@ if input.downcase == "basic"
     puts result
 elsif input.downcase == "advanced"
     puts "You have chosen the #{input} calculator"
+    puts "Please enter your value"
+    num_ad = gets.chomp.to_f
+    puts "If you are doing a square function please enter 'square' or if you are doing a root function please enter 'root'."
+    ad_op = gets.chomp
+    if ad_op.downcase == "square"
+        result = num_ad **2
+    elsif ad_op.downcase == "root"
+        result = num_ad **0.5
+    end
+    puts result
 else 
     puts "Please enter a valid option"
 end
